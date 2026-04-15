94103afc6a0540bcf9a735b5b1b96db67f142550
Riza Ega Satyabudhi <rizasatya@gmail.com>
2026-04-15 18:40:18 +0700

    feat: add github oauth

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,7 +3,37 @@ import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import notificationsPlugin from '@backstage/plugin-notifications/alpha';
 import signalsPlugin from '@backstage/plugin-signals/alpha';
 import { navModule } from './modules/nav';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
+import { SignInPageBlueprint } from '@backstage/plugin-app-react';
+import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
+
+const signInPage = SignInPageBlueprint.make({
+  params: {
+    loader: async () => props =>
+      (
+        <SignInPage
+          {...props}
+          provider={{
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          }}
+        />
+      ),
+  },
+});
 
 export default createApp({
-  features: [catalogPlugin, notificationsPlugin, signalsPlugin, navModule],
+  features: [
+    catalogPlugin,
+    notificationsPlugin,
+    signalsPlugin,
+    navModule,
+    createFrontendModule({
+      pluginId: 'app',
+      extensions: [signInPage],
+    }),
+  ],
 });
